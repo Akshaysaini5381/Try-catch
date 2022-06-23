bbcc09aff881ac2a22f817d6e91cff249bde5410
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-23 22:24:59 +0530

    Add project files.

---
+++ Try catch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace test_program
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.Write("Enter the first value :");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.Write("Enter the secound value :");
+                int num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(num1 + num2);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Pls enter the integar values :" + e);
+            }
+            finally
+            {
+                Console.WriteLine("YES :");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
